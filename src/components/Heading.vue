8d315dfffee3bf4e5e8c622502bb0f838369200c
Victor Butler <victorbutler@gmail.com>
2018-04-25 19:25:59 -0600

    rearrange Notification link in the nav

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -11,8 +11,8 @@
 
           <b-navbar-nav>
             <b-nav-item to="/Profits">Profits</b-nav-item>
-            <b-nav-item to="/Notifications">Notifications</b-nav-item>
             <b-nav-item to="/Balances">Balances</b-nav-item>
+            <b-nav-item to="/Notifications">Notifications</b-nav-item>
             <b-nav-item to="/Configure">Configure</b-nav-item>
           </b-navbar-nav>
 
