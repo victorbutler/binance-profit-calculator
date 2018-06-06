cbd085c05de8f9c60d74a3985f1c7773c5f9114a
Victor Butler <victorbutler@gmail.com>
2018-06-06 01:01:28 -0600

    materialize cards on the dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -2,10 +2,20 @@
   <div class="container">
     <h1>Dashboard - {{msg}}</h1>
     <hr />
-    <div class="card" v-for="marketObject in this.$parent.markets" v-bind:key="marketObject.id">
-      <h6 slot="header" class="mb-0">{{ marketObject.market }} Profits</h6>
-      <h4 :class="['card-text', ($store.state.history[marketObject.market].profitPlusBags > 0 ? 'text-success' : '')]">+Bags: {{ $store.state.history[marketObject.market].profitPlusBags }} {{ marketObject.market }}<br /><small>{{ $root.multiply($store.state.history[marketObject.market].profitPlusBags, $parent.getCoinMarketCapData(marketObject.market).price_usd) | accounting }}</small></h4>
-      <h4 :class="['card-text', ($store.state.history[marketObject.market].profitMinusBags > 0 ? 'text-success' : '')]">-Bags: {{ $store.state.history[marketObject.market].profitMinusBags }} {{ marketObject.market }}<br /><small>{{ $root.multiply($store.state.history[marketObject.market].profitMinusBags, $parent.getCoinMarketCapData(marketObject.market).price_usd) | accounting }}</small></h4>
+    <div class="row">
+      <div class="col s12 m6 l4 xl4" v-for="marketObject in this.$parent.markets" v-bind:key="marketObject.id">
+        <div class="card small">
+          <div class="card-content">
+            <span class="card-title"><router-link v-bind:to="('/Profits/' + marketObject.market)">{{ marketObject.market }} Profits</router-link></span>
+            <p>+Bags: {{ $store.state.history[marketObject.market].profitPlusBags }} {{ marketObject.market }} ({{ $root.multiply($store.state.history[marketObject.market].profitPlusBags, $parent.getCoinMarketCapData(marketObject.market).price_usd) | accounting }})</p>
+            <p>-Bags: {{ $store.state.history[marketObject.market].profitMinusBags }} {{ marketObject.market }} ({{ $root.multiply($store.state.history[marketObject.market].profitMinusBags, $parent.getCoinMarketCapData(marketObject.market).price_usd) | accounting }})</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Trade</a>
+            <a href="#">Activity</a>
+          </div>
+        </div>
+      </div>
     </div>
     <div v-if="this.$parent.markets.length === 0">
       <h2>There isn't any data yet. Head over to <b-link to="Configure">Configure</b-link> to supply your trade history.</h2>
